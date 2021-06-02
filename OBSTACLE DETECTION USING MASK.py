eee0b2635781538963c185648af1a50b626f0202
adi932001 <yadavadityaanil@gmail.com>
2021-06-02 19:36:40 +0530

    Project Intialised

---
+++ OBSTACLE DETECTION USING MASK.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np
+ref = cv.imread('F:/Python/ImgProc/butter.jpg')
+ref=cv.resize(ref,(int(319*2),int(224*2)))
+gaus = cv.GaussianBlur(ref,(5,5),2)
+hsvr = cv.cvtColor(ref, cv.COLOR_BGR2HSV)
+lower = np.array([110,50,50])
+upper = np.array([130,255,255])
+maskr = cv.inRange(hsvr, lower, upper)
+maskr0 = cv.dilate(maskr, None, iterations=3)
+maskr1 = cv.erode(maskr, None, iterations=2)
+cv.imshow("Image",ref)
+cv.imshow("Gaus",gaus)
+cv.imshow("Mask",maskr)
+cv.imshow("Dilate",maskr0)
+cv.imshow("Erode",maskr1)
+cv.waitKey(10)
